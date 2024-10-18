6a253f2b2577d1ec5f474d8a1f4aa8607d220bc0
shubhamsoni131202 <138276623+shubhamsoni131202@users.noreply.github.com>
2024-10-18 17:19:38 +0000

    add datatypes

---
+++ 01basics/01datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version
+
+let number = 21
+let Name = "shubham"
+let islogged = false
+
+console.log(number,Name,islogged);
+// console.log(Name);
+
+console.log("hello");
+
