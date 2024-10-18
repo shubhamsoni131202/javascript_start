728dbd9e771d60b25a365132f25171f70e01708e
shubhamsoni131202 <138276623+shubhamsoni131202@users.noreply.github.com>
2024-10-18 15:54:48 +0000

    datatypes

---
+++ 01basics/01variables.js
@@ -0,0 +1,15 @@
+const accountId = 14452   //const only used for declare constant term (it can not be edit later)
+let accountName = "shubham"   //let used for decleration of variables (it can be edit later)
+var accountEmail = "shubha@gmail"  //var used for decleration of variables (it can be edit later )[**but it is not recommended to use (old method and issue in block scope and function scope i.e. "{ }" )**]
+let accountState
+accountPassword = "11445"
+accountState="active"
+
+accountName ="soni"
+accountEmail = "abcd@gmail.com"
+
+
+// accountId = 4475
+
+console.log(accountId);
+console.table([accountName,accountEmail,accountState]);
