c555b30d99aed1339f1a89cae87adb6eb73e3154
shubhamsoni131202 <138276623+shubhamsoni131202@users.noreply.github.com>
2024-10-26 14:42:23 +0000

    strings and operations

---
+++ 01basics/01strings.js
@@ -0,0 +1,12 @@
+const name = " shubham"
+let repoName = "01basics"
+const testName = "javascript"
+const foodName = new String('bheel')
+const userState = "   chhattisgarh   "
+const userInfo = "shubham-soni-IT"
+
+console.log(userInfo.split("-"));
+
+// console.log(testName.includes("c++"));
+// console.log(userState.trim());
+// console.log(`my name is ${name.toUpperCase()} and my repo name is ${repoName} uses coding language = ${testName.slice(0,4)} , eats ${foodName}`);
