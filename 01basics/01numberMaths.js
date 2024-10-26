d4cf702bd8a2d99b8a7dbba9b9dc47cd282711fc
shubhamsoni131202 <138276623+shubhamsoni131202@users.noreply.github.com>
2024-10-26 15:43:07 +0000

    added number and maths

---
+++ 01basics/01numberMaths.js
@@ -0,0 +1,32 @@
+const score = 154
+const marks = new Number(35)
+const balance = 445
+const bill = 499.98
+const zeros = 10000000
+
+// console.log(zeros.toLocaleString("en-IN"));  //seperates zeros with commas in large value (*"em-IN"* is used for indian standards otherwise it uses us standards)
+
+// console.log(bill.toPrecision(3));
+// console.log(balance.toFixed(5));
+// console.log(`${score} ${marks}`);
+// console.log(marks);
+
+
+
+
+// ********************** MATHS ************************
+
+// console.log(Math)
+// console.log(Math.max(4,5,7,8));
+// console.log(Math.min(8,9,6,1));
+// console.log(Math.floor(4.987));
+// console.log(Math.ceil(4.2001));
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min +1) + min ));
+
+
