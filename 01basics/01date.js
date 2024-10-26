a928c8a93e6153a6b163847bd3743fb10e7e022e
shubhamsoni131202 <138276623+shubhamsoni131202@users.noreply.github.com>
2024-10-26 19:31:31 +0000

    date and timestamps

---
+++ 01basics/01date.js
@@ -0,0 +1,25 @@
+//time
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+
+let todayDate = new Date(2024,0,24)
+// console.log(todayDate.toDateString());
+
+/*
+    let myCreatedDate = new Date(26,1,2024)
+    output=> 8/17/1931;
+*/
+
+let myCreatedDate = new Date(2024,1,25)
+// console.log(myCreatedDate.toLocaleDateString());
+
+let dateNow = new Date(2024,10,26,11,25)
+// console.log(dateNow.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+console.log(myTimeStamp);
