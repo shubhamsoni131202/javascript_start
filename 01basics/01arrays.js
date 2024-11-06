80f001b05cc85f85b5d491b862494a451173db5b
shubhamsoni131202 <138276623+shubhamsoni131202@users.noreply.github.com>
2024-11-06 15:10:16 +0000

    array update

--- 01basics/01arrays.js
+++ 01basics/01arrays.js
@@ -3,15 +3,20 @@ let myArray = [0,1,2,3,4,5]
 
 
 let mr1 = new Array(0,1,2,3,4)
-console.log("A = " + mr1);
+// console.log("A = " + mr1);
 
 //in .slice method avoids elements of arrays from lower to upper-1 limit **but doesnt make any changes to original array.
 let mr2 = myArray.slice(1,3)
-console.log(mr2);
+// console.log(mr2);
 
 //in .splice method avoids elements of arrays from lower to upper limit **also it makes permanent changes in original array it permanently excludes elements according to limit from original array.
 let mr3 = myArray.splice(1,3)
-console.log("original array = " + myArray);
-console.log("B = " + mr3);
+// console.log("original array = " + myArray);
+// console.log("B = " + mr3);
 
- 
+let myArray2 = new Array(6,7,8,9,10)
+console.log(" original 2 = " + myArray2);
+let mr4 = myArray2.push(2)
+myArray2.push(5)
+myArray2.pop()
+console.log(myArray2);
